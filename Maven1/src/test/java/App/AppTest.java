6bfaf69883d0beb144248c7ec6424108374f4ecc
admst209 <admst209@DVWPNW0027.newmont.net>
2020-08-06 13:49:53 -0600

    Commit from Branch_maven

---
+++ Maven1/src/test/java/App/AppTest.java
@@ -0,0 +1,15 @@
+package App;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		
+		System.out.println("..This Test is Successful..");
+	}
+
+}
